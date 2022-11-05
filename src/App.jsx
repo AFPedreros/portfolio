f94f1d735624af4cf24f58470b68b08fd44dea7a
AFPedreros <felipe.pedreros94@gmail.com>
2022-11-05 14:23:12 -0500

    create project stack

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/Navbar"
+
+export default function App() {
+    return (
+        <div className="App h-screen bg-slate-500">
+            <Navbar />
+        </div>
+    )
+}
