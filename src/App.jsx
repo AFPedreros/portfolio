e06bc799417aff68e2165409115381a09e7503f3
AFPedreros <felipe.pedreros94@gmail.com>
2022-12-13 08:44:48 -0500

    change to nextjs

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "./components/Navbar"
-
-export default function App() {
-    return (
-        <div className="App h-screen bg-slate-500">
-            <Navbar />
-        </div>
-    )
-}
